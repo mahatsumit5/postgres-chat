088e11f5c9f14ecf711c333273fe9d5a4748a35e
Sumit Mahat <mahatsumit5@gmail.com>
2024-07-11 19:50:27 +1000

    error in createAuth token probably due to http

--- src/router/public.router.ts
+++ src/router/public.router.ts
@@ -35,8 +35,7 @@ router.post("/sign-in", async (req, res: Response, next) => {
     if (!isPasswordCorrect) {
       next(new Error("Incorrect Password"));
     }
-    // const token = await createAuth0Token(res);
-    const token = { access_token: "asdasdsdad" };
+    const token = await createAuth0Token(res);
     sessions[token.access_token] = req.body.email;
     return res.json({
       status: true,
